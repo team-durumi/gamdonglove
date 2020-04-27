5d192589816394fff2f2461642b807319bf13701
yeongtaek, Hong <mozodev@users.noreply.github.com>
2020-04-27 16:59:42 +0900

    [후원] 후원 등록 및 확인 #13 - 관리 후원 목록 화면을 추가합니다.

--- app/modules/custom/gd9_donate/src/Controller/Gd9DonateController.php
+++ app/modules/custom/gd9_donate/src/Controller/Gd9DonateController.php
@@ -3,11 +3,19 @@
 namespace Drupal\gd9_donate\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Database;
 
 /**
  * Returns responses for gd9_donate routes.
  */
 class Gd9DonateController extends ControllerBase {
+
+  public $fields = [];
+
+  public function __construct() {
+    $schema = \Drupal::config('gd9_donate.schema')->get('donations');
+    $this->fields = $schema['fields'];
+  }
 
   /**
    * Builds the response.
@@ -16,7 +24,7 @@ class Gd9DonateController extends ControllerBase {
 
     $regular_form = \Drupal::formBuilder()->getForm('Drupal\gd9_donate\Form\Gd9RegularDonateForm');
     $temp_form = \Drupal::formBuilder()->getForm('Drupal\gd9_donate\Form\Gd9TempDonateForm');
-    
+
     $build['content'] = [
       '#theme' => 'donate',
       '#data' => [
@@ -26,6 +34,50 @@ class Gd9DonateController extends ControllerBase {
     ];
 
     return $build;
+  }
+
+  public function admin() {
+    $headers = [
+      'did' => 'ID',
+      'type' => '유형',
+      'name' => '성명',
+      'amount' => '금액',
+      'need_reciept' => '영수증',
+      'created' => '신청일'
+    ];
+    $rows = [];
+
+    $sqlite = Database::getConnection('default', 'donation');
+    $results = $sqlite->query('SELECT * FROM gd9_donations ORDER BY created DESC LIMIT 0,10;')->fetchAll();
+    foreach ($results as $row_key => $result) {
+      foreach ($headers as $field_key => $field) {
+        $rows[$row_key][$field_key] = $result->$field_key;
+        if($field_key === 'type') {
+          $rows[$row_key][$field_key] = '정기';
+          if($result->$field_key == 2) $rows[$row_key][$field_key] = '일시';
+        }
+        if($field_key === 'need_reciept' && !empty($result->account_holder_ssn)) {
+          $rows[$row_key][$field_key] = '발급 필요';
+        }
+        if($field_key === 'created') {
+          $rows[$row_key][$field_key] = \Drupal::service('date.formatter')->format($rows[$row_key][$field_key], 'html_date');
+        }
+        if($field_key === 'amount') {
+          $rows[$row_key][$field_key] .= ' 만원';
+        }
+      }
+    }
+
+    $build['content'] = [
+      '#type' => 'table',
+      '#attributes' => ['class' => ['table']],
+      '#wrapper_attributes' => [ 'class' => [ 'table-responsive' ] ],
+      '#caption' => '후원 정보 목록',
+      '#header' => $headers,
+      '#rows' => $rows
+    ];
+    return $build;
+
   }
 
 }
