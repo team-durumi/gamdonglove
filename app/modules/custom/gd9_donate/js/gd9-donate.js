e0d976ca60267240bc878d5cf6928dc700c7dac3
yeongtaek, Hong <mozodev@users.noreply.github.com>
2020-06-29 09:49:19 +0900

    [고객요청] 후원 Print 눌렀을 때 화면에서 (후원하기- 프린트) #18

--- app/modules/custom/gd9_donate/js/gd9-donate.js
+++ app/modules/custom/gd9_donate/js/gd9-donate.js
@@ -1,27 +1,31 @@
 window.addEventListener('load', function() {
   var wrapper = document.getElementById("signature-pad");
-  var canvas = wrapper.querySelector("canvas");
-  var signaturePad = new SignaturePad(canvas, {
-    backgroundColor: 'rgb(255, 255, 255)'
-  });
+  if(wrapper) {
+    var canvas = wrapper.querySelector("canvas");
+    var signaturePad = new SignaturePad(canvas, {
+      backgroundColor: 'rgb(255, 255, 255)'
+    });
 
-  function resizeCanvas() {
-    var ratio =  Math.max(window.devicePixelRatio || 1, 1);
-    canvas.width = canvas.offsetWidth * ratio;
-    canvas.height = canvas.offsetHeight * ratio;
-    canvas.getContext("2d").scale(ratio, ratio);
-    // signaturePad.clear();
-  }
-  window.onresize = resizeCanvas;
-  resizeCanvas();
-
-  var form = document.getElementById('gd9-regular-donate-form');
-  var validation = form.addEventListener('submit', function(event) {
-    form.classList.add('was-validated');
-    if (form.checkValidity() === false) {
-      event.preventDefault(); event.stopPropagation(); return false;
+    function resizeCanvas() {
+      var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+      canvas.width = canvas.offsetWidth * ratio;
+      canvas.height = canvas.offsetHeight * ratio;
+      canvas.getContext("2d").scale(ratio, ratio);
+      // signaturePad.clear();
     }
-    const data = signaturePad.toDataURL("image/png");
-    document.getElementById('edit-signature').value = data;
-  }, false);
+    window.onresize = resizeCanvas;
+    resizeCanvas();
+  }
+  
+  var form = document.getElementById('gd9-regular-donate-form');
+  if(form) {
+    var validation = form.addEventListener('submit', function(event) {
+      form.classList.add('was-validated');
+      if (form.checkValidity() === false) {
+        event.preventDefault(); event.stopPropagation(); return false;
+      }
+      const data = signaturePad.toDataURL("image/png");
+      document.getElementById('edit-signature').value = data;
+    }, false);
+  }
 });
