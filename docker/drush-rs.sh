7aa0a0e91d30e95a101b492771632b5105d97ae8
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-01-15 14:00:36 +0900

    예전 개발 환경 관련 코드를 삭제합니다.

--- docker/drush-rs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
-echo "Current working directory: '"$(pwd)"'"
-docker-compose exec drupal drush rs 0.0.0.0:8888
--- docker/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm -rf /usr/local/bin/drush /usr/local/bin/drush-rs /usr/local/bin/composer
-chmod +x drush.sh drush-rs.sh composer.sh
-ln -s $(PWD)/drush.sh /usr/local/bin/drush
-ln -s $(PWD)/drush-rs.sh /usr/local/bin/drush-rs
-ln -s $(PWD)/composer.sh /usr/local/bin/composer
